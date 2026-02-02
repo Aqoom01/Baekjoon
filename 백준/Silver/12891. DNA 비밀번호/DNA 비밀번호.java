46366e310884ac6c82538e1ac58b7b3b5ba9865b
rlaush <84065399+Aqoom01@users.noreply.github.com>
2026-02-02 15:39:42 +0900

    [Silver II] Title: DNA 비밀번호, Time: 256 ms, Memory: 24428 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -1,61 +1,65 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
+	public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int S = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+        
+        char[] dna = br.readLine().toCharArray();
         st = new StringTokenizer(br.readLine());
-        int s = Integer.parseInt(st.nextToken());
-        int p = Integer.parseInt(st.nextToken());
-        char[] pw = br.readLine().toCharArray();
+        int A = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int G = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(st.nextToken());
+        
+        int a = 0;
+        int c = 0;
+        int g = 0;
+        int t = 0;
+        int count = 0;
+        for(int i = 0; i < dna.length; i++) {
+            switch(dna[i]) {
+                case 'A':
+                    a++;
+                    break;
+                case 'C':
+                    c++;
+                    break;
+                case 'G':
+                    g++;
+                    break;
+                case 'T':
+                    t++;
+                    break;
+            }
 
-        int[] check = new int[4];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < 4; i++) check[i] = Integer.parseInt(st.nextToken());
-
-        int[] curNum = new int[4];
-        int valid = 0;
-        int result = 0;
-
-        // 초기 윈도우
-        for (int i = 0; i < p; i++) {
-            int idx = charToIndex(pw[i]);
-            curNum[idx]++;
+            if(i == P - 1) if(a >= A && c >= C && g >= G && t >= T) count++;
+            if(i >= P) {
+                switch(dna[i - P]) {
+                    case 'A':
+                        a--;
+                        break;
+                    case 'C':
+                        c--;
+                        break;
+                    case 'G':
+                        g--;
+                        break;
+                    case 'T':
+                        t--;
+                        break;
+                }
+                if(a >= A && c >= C && g >= G && t >= T) count++;
+            }          
         }
-        for (int i = 0; i < 4; i++) if (curNum[i] >= check[i]) valid++;
-        if (valid == 4) result++;
-
-        // 슬라이딩 윈도우
-        for (int j = p; j < s; j++) {
-            int out = charToIndex(pw[j - p]);
-            int in = charToIndex(pw[j]);
-
-            // 나가는 문자 처리
-            if (curNum[out] >= check[out] && curNum[out] - 1 < check[out]) valid--;
-            curNum[out]--;
-
-            // 들어오는 문자 처리
-            curNum[in]++;
-            if (curNum[in] >= check[in] && curNum[in] - 1 < check[in]) valid++;
-
-            if (valid == 4) result++;
-        }
-
-
-        System.out.println(result);
-    }
-
-    //문자에 따른 인덱스 매핑
-    static int charToIndex(char c) {
-        switch (c) {
-            case 'A': return 0;
-            case 'C': return 1;
-            case 'G': return 2;
-            case 'T': return 3;
-        }
-        return -1;
-    }
+        
+        bw.write("" + count);
+        bw.flush();
+        bw.close();
+	}
 }
