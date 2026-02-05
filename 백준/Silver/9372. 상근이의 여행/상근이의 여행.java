45c17c4be240ce5818ea9a508eab998ab2b614bf
rlaush <84065399+Aqoom01@users.noreply.github.com>
2026-02-05 09:40:52 +0900

    [Silver IV] Title: 상근이의 여행, Time: 224 ms, Memory: 25136 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int T = Integer.parseInt(br.readLine());
+        for(int t = 1; t <= T; t++) {
+        	StringTokenizer st = new StringTokenizer(br.readLine());
+        	int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            for(int i = 0; i < M; i++) br.readLine();
+        	bw.write(N - 1 + "\n");
+        }
+        
+        bw.flush();
+        bw.close();
+    }
+}
